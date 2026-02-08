47880c29bd6fd662848713e0a54e0c8f82f31cf7
aMadAlien <irasibil2@gmail.com>
2026-02-08 21:16:16 +0200

    setup deploy config

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,7 +1,8 @@
 import type { Config } from "@react-router/dev/config";
 
+const basename = process.env.BASE_PATH || "/";
+
 export default {
-  // Config options...
-  // Server-side render by default, to enable SPA mode set this to `false`
-  ssr: true,
+  ssr: false,
+  basename,
 } satisfies Config;
