b42c638ab7436b46b6d58084222b1fa154f3b369
aMadAlien <irasibil2@gmail.com>
2026-02-13 15:45:29 +0200

    update deply configs

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   basePath: process.env.NEXT_PUBLIC_BASE_PATH || "",
-  output: "standalone",
+  output: (process.env.BUILD_OUTPUT as "standalone" | "export") || "standalone",
   images: {
     remotePatterns: [
       {
